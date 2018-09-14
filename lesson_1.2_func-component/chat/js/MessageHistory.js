a54a6e56239f8208980893f24b581c9f09eca608
Denis Zhebov <zhebov@gmail.com>
2018-09-14 12:42:42 +0300

    Task 2 solution

--- lesson_1.2_func-component/chat/js/MessageHistory.js
+++ lesson_1.2_func-component/chat/js/MessageHistory.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function MessageHistory({ list }) {
+
+  const messages = list.map(item => {
+    return (
+      (item.type === 'message' && <Message from={item.from} message={item} />) ||
+      (item.type === 'response' && <Response from={item.from} message={item} />) ||
+      (item.type === 'typing' && <Typing from={item.from} message={item} />)
+    )
+  })
+  
+  return (
+    <ul>
+      {messages};
+    </ul>
+  )
+}
