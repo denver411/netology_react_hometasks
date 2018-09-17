eb106ce113b26d30df96de473f79a9e581752352
Denis Zhebov <zhebov@gmail.com>
2018-09-17 14:23:05 +0300

    Task 3 solution

--- lesson_1.3_events/auth/js/AuthForm.js
+++ lesson_1.3_events/auth/js/AuthForm.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const AuthForm = ({onAuth}) => {
+const AuthForm = ({ onAuth }) => {
 
-  function onSubmit (event){
+  function onSubmit(event) {
     event.preventDefault();
     const elements = event.currentTarget.elements;
     const formData = {
@@ -10,32 +10,39 @@ const AuthForm = ({onAuth}) => {
       email: elements[1].value,
       password: elements[2].value
     }
-    console.log(formData);
-    // if (onAuth && typeof(onAuth) === 'function'){
-    //   onAuth(formData);
-    // }
-    
+    if (onAuth && typeof(onAuth) === 'function'){
+      onAuth(formData);
+    }
   }
 
-return (
-  <form onSubmit={onSubmit} class="ModalForm" action="/404/auth/" method="POST">
-  <div className="Input">
-    <input required type="text" placeholder="Имя" />
-    <label></label>
-  </div>
-  <div className="Input">
-    <input type="email" placeholder="Электронная почта" />
-    <label></label>
-  </div>
-  <div className="Input">
-    <input required type="password" placeholder="Пароль" />
-    <label></label>
-  </div>
-  <button type="submit">
-    <span>Войти</span>
-    <i className="fa fa-fw fa-chevron-right"></i>
-  </button>
-  </form>
-)
+  function getEmail(event){
+    const field = event.currentTarget;
+    field.value = (field.value).match(/[a-z0-9_\.\-@]+/gi);
+  }
 
+ function getPass(event){
+    const field = event.currentTarget;
+    field.value = (field.value).match(/[a-z0-9_]+/gi);
+  }
+
+  return (
+    <form onSubmit={onSubmit} class="ModalForm" action="/404/auth/" method="POST">
+      <div className="Input">
+        <input required type="text" placeholder="Имя" />
+        <label></label>
+      </div>
+      <div className="Input">
+        <input onChange={getEmail} type="email" placeholder="Электронная почта" />
+        <label></label>
+      </div>
+      <div className="Input">
+        <input onChange={getPass} required type="password" placeholder="Пароль" />
+        <label></label>
+      </div>
+      <button type="submit">
+        <span>Войти</span>
+        <i className="fa fa-fw fa-chevron-right"></i>
+      </button>
+    </form>
+  )
 }
