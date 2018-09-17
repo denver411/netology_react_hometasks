3b9dc78d2939dbcb800b122df7ba2d75435c25d8
Denis Zhebov <zhebov@gmail.com>
2018-09-17 06:12:40 +0300

    Task 3 part result

--- lesson_1.3_events/auth/js/AuthForm.js
+++ lesson_1.3_events/auth/js/AuthForm.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const AuthForm = ({onAuth}) => {
+
+  function onSubmit (event){
+    event.preventDefault();
+    const elements = event.currentTarget.elements;
+    const formData = {
+      name: elements[0].value,
+      email: elements[1].value,
+      password: elements[2].value
+    }
+    console.log(formData);
+    // if (onAuth && typeof(onAuth) === 'function'){
+    //   onAuth(formData);
+    // }
+    
+  }
+
+return (
+  <form onSubmit={onSubmit} class="ModalForm" action="/404/auth/" method="POST">
+  <div className="Input">
+    <input required type="text" placeholder="Имя" />
+    <label></label>
+  </div>
+  <div className="Input">
+    <input type="email" placeholder="Электронная почта" />
+    <label></label>
+  </div>
+  <div className="Input">
+    <input required type="password" placeholder="Пароль" />
+    <label></label>
+  </div>
+  <button type="submit">
+    <span>Войти</span>
+    <i className="fa fa-fw fa-chevron-right"></i>
+  </button>
+  </form>
+)
+
+}
