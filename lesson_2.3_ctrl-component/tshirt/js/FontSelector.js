f1f0c5a64fd6385f8af979db704ce5b82ec2c37d
Denis Zhebov <zhebov@gmail.com>
2018-09-25 22:17:46 +0300

    Task 3 solution

--- lesson_2.3_ctrl-component/tshirt/js/FontSelector.js
+++ lesson_2.3_ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,30 @@
-const FontSelector = ({fonts, selectedFont, onSelect}) => {
-    return (
-        <div className="font-picker">
-            Выберите шрифт
-        </div>
-    )
+const FontSelector = props => {
+  const handleChange = event => {
+    const selectedFont = props.fonts.filter(
+      item => item.name === event.currentTarget.value
+    )[0];
+    props.onSelect(selectedFont);
+  };
+
+  return (
+    <div className="font-picker">
+      {props.fonts.map(item => {
+        return (
+          <div className="grid center font-item">
+            <input
+              onChange={handleChange}
+              type="radio"
+              selected={item}
+              value={item.name}
+              name="font"
+              id={item.name}
+            />
+            <label htmlFor={item.name} class="grid-1">
+              <PictureFont text={item.name.slice(0, 3)} path={item.path} />
+            </label>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
--- lesson_2.3_ctrl-component/tshirt/js/TextRenderLine.js
+++ lesson_2.3_ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,18 @@
-const TextRenderLine = ({value, onChange}) => {
-	return (
-		<div className="type-text">
-			Наберите текст
-		</div>
-	);
+const TextRenderLine = ({ value, onChange }) => {
+  const handleChange = event => {
+    onChange(event.currentTarget.value);
+  };
+  return (
+    <div className="type-text">
+      <textaЕфыrea
+        value={value}
+        onChange={handleChange}
+        name="text"
+        id="font-text"
+        cols="30"
+        rows="2"
+        placeholder="Введите текст для футболкиЕфы"
+      />
+    </div>
+  );
 };
