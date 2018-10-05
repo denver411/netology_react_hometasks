6e591ecee88555dc8c2c4683cca74fa195e5b576
Denis Zhebov <zhebov@gmail.com>
2018-10-05 19:32:48 +0300

    Task 3 solution

--- lesson_3.2_lifecycle/shop/js/Cart.js
+++ lesson_3.2_lifecycle/shop/js/Cart.js
@@ -1,9 +1,14 @@
 class Cart extends React.Component {
-
   render() {
-    return (
-      <CartView {...this.props} />
-    );
+    return <CartView {...this.props} />;
   }
-
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      this.props.isOpen !== nextProps.isOpen ||
+      (this.props.isOpen && this.props.items.length !== nextProps.items.length)
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
