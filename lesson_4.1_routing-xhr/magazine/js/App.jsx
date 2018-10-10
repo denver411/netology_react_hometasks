100f277d39b98dd60710bd12dce3996dd12fb6ce
Denis Zhebov <zhebov@gmail.com>
2018-10-10 18:14:15 +0300

    Task 1 improvements

--- lesson_4.1_routing-xhr/magazine/js/App.jsx
+++ lesson_4.1_routing-xhr/magazine/js/App.jsx
@@ -3,15 +3,15 @@ const { BrowserRouter, Route, Switch, Link } = ReactRouter;
 
 class App extends React.Component {
   render() {
-    return (     
-        <div>
-          <Nav />
-          <Switch>
-            <Route path="/magazine/subscribtion" component={SubscribtionPage} />
-            <Route path="/magazine/article/:id" component={ArticlePage} />
-            <Route path="/magazine" component={Homepage} />
-          </Switch>
-        </div>
+    return (
+      <div>
+        <Nav />
+        <Switch>
+          <Route path="/magazine/subscribtion" component={SubscribtionPage} />
+          <Route path="/magazine/article/:id" component={ArticlePage} />
+          <Route path="/magazine" component={Homepage} />
+        </Switch>
+      </div>
     );
   }
 }
