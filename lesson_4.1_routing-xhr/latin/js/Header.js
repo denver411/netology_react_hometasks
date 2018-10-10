1a331f837d0d5bedb03e63cda0bbee580110c3b8
Denis Zhebov <zhebov@gmail.com>
2018-10-10 18:01:09 +0300

    Task 3 solution

--- lesson_4.1_routing-xhr/latin/js/Header.js
+++ lesson_4.1_routing-xhr/latin/js/Header.js
@@ -1,11 +1,17 @@
-const HeaderComponent = ({ match }) => (
-  <nav className="navbar navbar-light bg-light">
-    {match && match.params.id
-      ? <p className="navbar-brand">Уникальный идентификатор статьи: { match.params.id}</p>
-      : <p className="navbar-brand">Статья не выбрана</p> 
-    }
-  </nav>
-);
+const HeaderComponent = ({ location }) => {
+  const articleId = location.pathname.match(/\d+$/);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      {articleId ? (
+        <p className="navbar-brand">
+          Уникальный идентификатор статьи: {articleId}
+        </p>
+      ) : (
+        <p className="navbar-brand">Статья не выбрана</p>
+      )}
+    </nav>
+  );
+};
 
-const {withRouter} = window.ReactRouterDOM;
+const { withRouter } = window.ReactRouterDOM;
 const Header = withRouter(HeaderComponent);
