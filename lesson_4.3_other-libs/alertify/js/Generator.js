450f1531ec0a989c50050b2cd49e0cf0fddfb745
Denis Zhebov <zhebov@gmail.com>
2018-10-17 20:18:51 +0300

    Task 2 solution

--- lesson_4.3_other-libs/alertify/js/Generator.js
+++ lesson_4.3_other-libs/alertify/js/Generator.js
@@ -1,10 +1,9 @@
 class Site extends React.Component {
-
   constructor(props) {
     super(props);
-
+    this.alertify = window.alertify;
     this.state = {
-      ids: [],
+      ids: []
     };
 
     this.generate = this.generate.bind(this);
@@ -14,6 +13,34 @@ class Site extends React.Component {
     this.generate();
   }
 
+  componentDidUpdate() {
+    const newId = this.state.ids[this.state.ids.length - 1];
+    const lengthId = newId.length;
+    this.alertify.log(`Новый ID ${lengthId} символ${this.getAlertEnding(lengthId)}`, null, 10000);
+  }
+
+  getAlertEnding = lengthId => {
+    let lastDigit = [].slice.call(new String(lengthId), -1)[0];
+    const ending = {
+      0: 'ов',
+      1: '',
+      2: 'а',
+      3: 'а',
+      4: 'а',
+      5: 'ов',
+      6: 'ов',
+      7: 'ов',
+      8: 'ов',
+      9: 'ов'
+    };
+
+    if (lengthId >10 && lengthId < 15) {
+      lastDigit = 0;
+    }
+
+    return ending[lastDigit];
+  };
+
   render() {
     return (
       <div className="row">
@@ -21,11 +48,15 @@ class Site extends React.Component {
           <h1>Генератор случайных идентификаторов</h1>
         </header>
         <aside className="menu col-sm-12 col-md-4">
-          <button className="button" onClick={this.generate}>Получить новый идентификатор</button>
+          <button className="button" onClick={this.generate}>
+            Получить новый идентификатор
+          </button>
         </aside>
         <main className="ids col-sm-12 col-md-8">
           <ul>
-            {this.state.ids.map(id => <li key={id}>{id}</li>)}
+            {this.state.ids.map(id => (
+              <li key={id}>{id}</li>
+            ))}
           </ul>
         </main>
       </div>
@@ -36,22 +67,24 @@ class Site extends React.Component {
     const newId = makeid(random(5, 43, false));
 
     this.setState(prevState => ({
-      ids: [...prevState.ids, newId],
+      ids: [...prevState.ids, newId]
     }));
   }
 }
 
 const makeid = (length = 12) => {
-  let text = "";
-  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = '';
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
   for (let i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
 
   return text;
-}
+};
 
-const random = (lower = 0, upper = 1, floating = true) => floating
-  ? Math.min(lower + (Math.random() * (upper - lower)), upper)
-  : lower + Math.floor(Math.random() * (upper - lower + 1))
+const random = (lower = 0, upper = 1, floating = true) =>
+  floating
+    ? Math.min(lower + Math.random() * (upper - lower), upper)
+    : lower + Math.floor(Math.random() * (upper - lower + 1));
