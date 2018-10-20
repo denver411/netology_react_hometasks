77affc223e6eab37aad96819b64243c9af3de564
Denis Zhebov <zhebov@gmail.com>
2018-10-20 10:01:46 +0300

    Иправление замечаний после проверки

--- lesson_2.4_typechecking/components-doc/js/DateInput.js
+++ lesson_2.4_typechecking/components-doc/js/DateInput.js
@@ -6,7 +6,7 @@ const DateInput = props => {
     <div className="form-group">
       <label>{props.label}</label>
       <input
-        type="text"
+        type={props.type}
         className="form-control"
         name={props.name}
         onChange={props.onChange}
@@ -19,6 +19,7 @@ const DateInput = props => {
 };
 
 DateInput.propTypes = {
+  type: PropTypes.string.isRequired,
   label:PropTypes.string,
   name: PropTypes.string,
   onChange: PropTypes.func,
